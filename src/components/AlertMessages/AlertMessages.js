dcaa82f1fbb8d06985cc83eebafc84fff168ccd3
Myron <dubtwee@gmail.com>
2020-08-01 14:47:56 -0500

    adding a button to be able to close the error alert

--- src/components/AlertMessages/AlertMessages.js
+++ src/components/AlertMessages/AlertMessages.js
@@ -4,12 +4,17 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 import styles from './AlertMessages.module.css';
 
 class AlertMessages extends Component {
+  closeError = (event) => {
+    this.props.dispatch({ type: 'CLEAR_ERROR' });
+  }
+
   render() {
     return (
       <div className={styles.alertFrame}>
         {this.props.store.error.message &&
           <div className={styles.alertPanel}>
             {this.props.store.error.message}
+            <button onClick={this.closeError}>close</button>
           </div>
         }
       </div>
--- src/redux/reducers/error.reducer.js
+++ src/redux/reducers/error.reducer.js
@@ -1,15 +1,17 @@
-const defaultMessage = {
+const defaultError = {
   errorMsg: null,
 };
 
-function errorReducer(state = defaultMessage, action) {
+function errorReducer(state = defaultError, action) {
   switch (action.type) {
     case 'ERROR_MESSAGE':
       return {
         message: action.payload,
       };
+    case 'CLEAR_ERROR':
+      return defaultError;
     default:
-      return defaultMessage;
+      return defaultError;
   }
 }
 
