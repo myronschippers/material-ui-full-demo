d749742c83c5cd5ea1369b01567b3e8d89dd523b
Myron <dubtwee@gmail.com>
2020-08-01 08:23:30 -0500

    finalizing query to get back all data for creature details

--- server/routes/creatures.router.js
+++ server/routes/creatures.router.js
@@ -19,14 +19,30 @@ router.get('/', (req, res) => {
 });
 
 router.get('/details/:id', (req, res) => {
-  const queryText = `SELECT * FROM "creatures"
-    JOIN "type" ON creatures.type_id = type.id
-    JOIN "creatures_attributes" ON creatures.id = creatures_attributes.creatures_id
-    JOIN "creatures_habitat" ON creatures.id = creatures_habitat.creatures_id
-    WHERE "id" = $1;`;
-    pool.query(queryText)
+  const queryText = `SELECT creatures.*,
+      array_agg(attributes.tag) as attributes,
+      (
+        SELECT type.label as type_label FROM creatures
+        JOIN type ON type.id = creatures.type_id
+        WHERE creatures.id = $1
+      ),
+      (
+        SELECT array_agg(habitat.label) as habitats FROM creatures
+        LEFT JOIN creatures_habitat ON creatures.id = creatures_habitat.creatures_id
+        LEFT JOIN habitat ON creatures_habitat.habitat_id = habitat.id
+        WHERE creatures.id = $1
+        GROUP BY creatures.id
+      )
+    FROM creatures
+    LEFT JOIN creatures_attributes ON creatures.id = creatures_attributes.creatures_id
+    LEFT JOIN attributes ON creatures_attributes.attributes_id = attributes.id
+    WHERE creatures.id = $1
+    GROUP BY creatures.id;`;
+  const creatureId = req.params.id;
+
+  pool.query(queryText, [creatureId])
     .then((dbResponse) => {
-      res.send(dbResponse.rows[0]);
+      res.send(dbResponse.rows);
     })
     .catch((err) => {
       console.log(err);
